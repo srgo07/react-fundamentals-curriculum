9779174bd64c64d9ce2847e553d477cdcabcdfe1
srgo07 <srgo07.per@gmail.com>
2019-04-09 20:19:04 -0400

    adding some event handlers

--- app/containers/GetWeather.js
+++ app/containers/GetWeather.js
@@ -6,13 +6,30 @@ const {string} = require('prop-types');
 class GetWeather extends React.Component {
     constructor (props) {
         super(props);
+        this.state = {
+            location: "Santiago"
+        }
+        this.handleInputText = this.handleInputText.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
+
+    handleInputText (event) {
+        console.log(event.target.value);
+        this.setState({
+            location: event.target.value
+        })
+    }
+
+    handleSubmit() {
+        // sent to axios the city name
+    }
+
     render() {
         return (
             <div className={'get-weather-component' + this.props.direction}
                 style={{flexDirection: this.props.direction}}>
-                <input className="location-input" placeholder="St. George, Utah" />
-                <button className="get-weather" >Get Weather</button>
+                <input className="location-input"  onChange={this.handleInputText} placeholder="St. George, Utah" />
+                <button className="get-weather" onClick={this.handleSubmit} >Get Weather</button>
             </div>
         );
     }
